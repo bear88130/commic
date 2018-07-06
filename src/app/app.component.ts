25b16bff182c6dcb765bbee2ab69a6fed1a5c527
bear88130 <bear88130@yahoo.com.tw>
2018-07-06 20:50:14 +0800

    點擊換頁換圖

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Output, Input, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @Input() nowChapter: number;
+  @Input() imgNum: number;
+  @Output()
+  nowPage: number;
+  nowChapterPage = 0;
+  arrayChapter: number;
+
   chapter = [ [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12] , [13 , 14 , 15 ] ];
-  nowChapter = 1;
-  nowChapterPage = 1;
-  imgNum = this.chapter[this.nowChapter][this.nowChapterPage];
-  nowPage = 1;
+
+  ngOnInit() {
+    this.nowPage = 1;
+    this.nowChapter = 1;
+    this.arrayChapter = this.nowChapter - 1;
+    this.imgNum = this.chapter[this.arrayChapter][this.nowChapterPage];
+  }
 
   showChapter(num) {
     this.nowChapter = num;
+    this.arrayChapter = this.nowChapter - 1;
+    this.imgNum = this.chapter[this.arrayChapter][this.nowChapterPage];
+
     this.nowPage = 2;
   }
 
--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
-
+import { Component, OnInit, EventEmitter, Output, Input,  } from '@angular/core';
 @Component({
   selector: 'app-read',
   templateUrl: './read.component.html',
-  styleUrls: ['./read.component.css']
+  styleUrls: ['./read.component.css'],
 })
 export class ReadComponent implements OnInit {
-  @Input()
+  @Input() nowChapter: string;
+  @Input() imgNum: number;
   @Output()
   turnBackEvent = new EventEmitter();
-
-  constructor() {}
-
+  pageString: string;
+  chapterString: string;
+  constructor() { }
+  chapterNum = 1;
+  pageNum = 1;
+  imgSrc = '';
   ngOnInit() {
-    (<HTMLInputElement>document.getElementById('chapter')).value = '2';
-    (<HTMLInputElement>document.getElementById('page')).value = '1';
+    this.chapterString = String(this.nowChapter);
+    this.pageString = String(this.pageNum);
+    (<HTMLInputElement>document.getElementById('chapter')).value = this.chapterString;
+    (<HTMLInputElement>document.getElementById('page')).value = this.pageString;
+    this.imgSrc = './image/storyboard-' + this.imgNum + '.png';
+  }
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnChanges() {
+    (<HTMLInputElement>document.getElementById('chapter')).value = this.chapterString;
+    (<HTMLInputElement>document.getElementById('page')).value = this.pageString;
   }
 
   turnBack() {
