7fc0aa2c3a51dfb6bb194525b65f35e655d604f2
bear88130 <bear88130@yahoo.com.tw>
2018-07-08 22:51:55 +0800

    完成固定的資料繫結

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,15 @@ import { Component, Output, Input, OnInit, OnChanges } from '@angular/core';
 export class AppComponent implements OnInit {
   @Input() nowChapter: number;
   @Input() imgNum: number;
+  @Input() chapter: number[][];
   @Output()
   nowPage: number;
   nowChapterPage = 0;
   arrayChapter: number;
 
-  chapter = [ [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12] , [13 , 14 , 15 ] ];
 
   ngOnInit() {
+    this.chapter = [ [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12] , [13 , 14 , 15 , 16 , 17 , 18 , 19 , 20 , 21 , 22 , 23 , 24] ];
     this.nowPage = 1;
     this.nowChapter = 1;
     this.arrayChapter = this.nowChapter - 1;
--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output, Input,  } from '@angular/core';
+import { element } from 'protractor';
 @Component({
   selector: 'app-read',
   templateUrl: './read.component.html',
@@ -7,29 +8,97 @@ import { Component, OnInit, EventEmitter, Output, Input,  } from '@angular/core'
 export class ReadComponent implements OnInit {
   @Input() nowChapter: string;
   @Input() imgNum: number;
-  @Output()
-  turnBackEvent = new EventEmitter();
+  @Input() chapterArray: number[][];
+  @Output() turnBackEvent = new EventEmitter();
+
   pageString: string;
   chapterString: string;
+  chapterNum: number;
+  arrayChapterPage: number[];
+  arrayChapterPageName: string[];
+  pageNum: number;
+  imgSrc: string;
+  dropdownC: string;
+  dropdownP: string;
+  numC: number;
+  numP: number;
+
   constructor() { }
-  chapterNum = 1;
-  pageNum = 1;
-  imgSrc = '';
+
   ngOnInit() {
+    this.arrayChapterPage = this.chapterArray[Number(this.nowChapter) - 1];
+    this.chapterNum = 1;
+    this.pageNum = 0;
     this.chapterString = String(this.nowChapter);
     this.pageString = String(this.pageNum);
-    (<HTMLInputElement>document.getElementById('chapter')).value = this.chapterString;
-    (<HTMLInputElement>document.getElementById('page')).value = this.pageString;
-    this.imgSrc = './image/storyboard-' + this.imgNum + '.png';
-  }
-
-  // tslint:disable-next-line:use-life-cycle-interface
-  ngOnChanges() {
-    (<HTMLInputElement>document.getElementById('chapter')).value = this.chapterString;
-    (<HTMLInputElement>document.getElementById('page')).value = this.pageString;
+    (<HTMLSelectElement>document.getElementById('chapter')).value = this.chapterString;
+    (<HTMLInputElement>document.getElementById('page')).value = '1';
+    this.changeImg();
   }
 
   turnBack() {
     this.turnBackEvent.emit();
+  }
+
+  changeChapter($event) {
+    if ($event.target.value === '0') {
+      alert('請選擇章數');
+    } else {
+      this.changeImg();
+    }
+  }
+
+  changeChapterPage($event) {
+    if ($event.target.value === '0') {
+      alert('請選擇頁數');
+    } else {
+      this.changeImg();
+    }
+  }
+
+  prePage() {
+    const page = <HTMLSelectElement>document.getElementById('page');
+    if (page.value === '1') {
+      alert('第一頁');
+    } else {
+      page.value = String(Number(page.value) - 1);
+      this.changeImg();
+    }
+  }
+
+  nextPage() {
+    const page = <HTMLSelectElement>document.getElementById('page');
+    if (page.value === String(page.length - 1)) {
+      alert('最後一頁');
+    } else {
+      page.value = String(Number(page.value) + 1);
+    }
+    this.changeImg();
+  }
+
+    turnPick(num) {
+      const page = <HTMLSelectElement>document.getElementById('page');
+      page.value = String(num);
+      this.changeImg();
+    }
+
+    changeImg() {
+    // 顯示下拉選單的值
+    this.dropdownC = (<HTMLSelectElement>document.getElementById('chapter')).value;
+    this.dropdownP = (<HTMLInputElement>document.getElementById('page')).value;
+    // 陣列的編號
+    this.numC = Number((<HTMLSelectElement>document.getElementById('chapter')).value) - 1;
+    this.numP = Number((<HTMLInputElement>document.getElementById('page')).value) - 1;
+    // 圖片陣列取得的值
+    this.imgNum = this.chapterArray[this.numC][this.numP];
+    this.imgSrc = './image/storyboard-' + this.imgNum + '.png';
+  }
+
+  TestSelected(num) {
+    if (num === this.numP + 1) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
